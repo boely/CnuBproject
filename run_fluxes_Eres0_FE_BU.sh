36563b131971789080704931a5b81c8752a84244
Muller <rasa.muller@tno.nl>
2018-01-10 14:45:12 +0100

    added bash scripts to createand analyse fluxes (F), and flux timed E to the power -alpha (FE) for different situations bottom up (BU), top down (TD), and optimal (OPT), and different detector energy resolutions (Eres0), (Eres30), and (Eres80)

---
+++ run_fluxes_Eres0_FE_BU.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# run a NikhefORCA job on the stoomboot cluster
+
+callcommand="$0 $*"
+
+#PROG=$1
+#echo "prog = ${PROG}"
+
+#TARGETDIR=$2
+#echo "output directory = '${TARGETDIR}'"
+
+#ARGS=$3
+#echo "args = '${ARGS}'"
+
+echo ""
+
+CALLDIR=`pwd` # directory from which this script was called
+QUEUE=generic # can also be e.g. express, generic, long
+
+# create script
+SCRIPTNAME=tmp_script_$$.sh
+cat >> $SCRIPTNAME << EOF
+#!/bin/bash -l
+
+export PATH=/project/datagrid/anaconda/bin:$PATH
+##source activate py27root5 #doesnt seem to work
+source activate py27root5
+export PATH=/project/datagrid/anaconda/bin:$PATH
+
+# print working directory
+cd \$TMPDIR
+echo "Working directory = " 
+pwd
+echo ""
+
+echo "Directory contents: "
+ls
+echo ""
+
+# copy necessary input files
+echo "Copying input files"
+cp $CALLDIR/src/NeutrinoFlux.py . # the program itself
+cp $CALLDIR/create_fluxes.py . # the program itself
+cp $CALLDIR/src/Statistics.py . # the program itself
+cp $CALLDIR/config.m4 .  # config files
+
+echo "Directory contents: "
+ls
+echo ""
+
+# run the program
+echo "Running"
+
+m4 -D ER=0 -D N=4 -D Z=2 -D TF=True config.m4 > ERES0_FE_BU.cfg
+python create_fluxes.py ERES0_FE_BU.cfg ERES0_FE_BU
+python Statistics.py ERES0_FE_BU_H0H1_m0.1_zmax2_n4_alpha2_ZdecayTrue.txt
+
+echo "Directory contents: "
+ls
+echo ""
+
+# copy output files to output directory
+echo "Copying output files."
+cp *ERES0_FE_BU* ${CALLDIR}/.
+
+EOF
+
+echo "created ${SCRIPTNAME}, now submitting to queue."
+
+# change access rights
+chmod u=rwx ${SCRIPTNAME}
+
+# submit script
+qsub -N CnuB_fluxes_ERES0_FE_BU -q ${QUEUE} -V -j oe  -m be -M rasam@nikhef.nl ./$SCRIPTNAME
+
+# remove original script
+rm ${SCRIPTNAME}
+
+
---
+++ run_fluxes_Eres30_F_TD.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# run a NikhefORCA job on the stoomboot cluster
+
+callcommand="$0 $*"
+
+#PROG=$1
+#echo "prog = ${PROG}"
+
+#TARGETDIR=$2
+#echo "output directory = '${TARGETDIR}'"
+
+#ARGS=$3
+#echo "args = '${ARGS}'"
+
+echo ""
+
+CALLDIR=`pwd` # directory from which this script was called
+QUEUE=generic # can also be e.g. express, generic, long
+
+# create script
+SCRIPTNAME=tmp_script_$$.sh
+cat >> $SCRIPTNAME << EOF
+#!/bin/bash -l
+
+export PATH=/project/datagrid/anaconda/bin:$PATH
+##source activate py27root5 #doesnt seem to work
+source activate py27root5
+export PATH=/project/datagrid/anaconda/bin:$PATH
+
+# print working directory
+cd \$TMPDIR
+echo "Working directory = " 
+pwd
+echo ""
+
+echo "Directory contents: "
+ls
+echo ""
+
+# copy necessary input files
+echo "Copying input files"
+cp $CALLDIR/src/NeutrinoFlux.py . # the program itself
+cp $CALLDIR/create_fluxes.py . # the program itself
+cp $CALLDIR/src/Statistics.py . # the program itself
+cp $CALLDIR/config.m4 .  # config files
+
+echo "Directory contents: "
+ls
+echo ""
+
+# run the program
+echo "Running"
+
+m4 -D ER=0 -D N=2 -D Z=20 -D TF=False config.m4 > ERES30_F_TD.cfg
+python create_fluxes.py ERES30_F_TD.cfg ERES30_F_TD
+python Statistics.py ERES30_F_TD_H0H1_m0.1_zmax20_n2_alpha2_ZdecayTrue.txt
+
+echo "Directory contents: "
+ls
+echo ""
+
+# copy output files to output directory
+echo "Copying output files."
+cp *ERES30_F_TD* ${CALLDIR}/.
+
+EOF
+
+echo "created ${SCRIPTNAME}, now submitting to queue."
+
+# change access rights
+chmod u=rwx ${SCRIPTNAME}
+
+# submit script
+qsub -N CnuB_fluxes_ERES30_F_TD -q ${QUEUE} -V -j oe  -m be -M rasam@nikhef.nl ./$SCRIPTNAME
+
+# remove original script
+rm ${SCRIPTNAME}
+
+
